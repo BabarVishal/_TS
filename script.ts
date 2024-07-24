7baa3eef40f8e4707bd7a75811846af71d51fe49
vishal <vishalbabar7351@gmail.com>
2024-07-24 23:28:17 +0530

    first commit

---
+++ script.ts
@@ -0,0 +1,20 @@
+// Basic Types
+// .Number, String, Boolean
+// Arrays, tuples
+// Any, unknown, never, void
+// Enum
+let a = 22;
+let b = 33;
+
+let variable : [2, 3, 4, 5, 6];
+let arr = [1, 2, 3, 4];
+
+
+
+
+
+
+
+
+
+
